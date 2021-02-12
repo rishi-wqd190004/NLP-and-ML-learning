ec97913cbc73f621a4f7b3b8d75bf14688b8571a
rishi-wqd190004 <wqd190004@siswa.um.edu.my>
2021-02-12 15:10:09 +0800

    adding SGD

--- ML_learning/training_linear_models/linear_regression_normal_equation.py
+++ ML_learning/training_linear_models/linear_regression_normal_equation.py
@@ -1,5 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.linear_model import SGDRegressor
+
 # random x and y numbers
 x = 2 * np.random.rand(100,1)
 y = 4 + 3 * x + np.random.randn(100,1) #y=4+3x_1+gaussian's noise
@@ -29,6 +31,7 @@ plt.axis([0, 2, 0, 15])
 plt.show()
 
 ## applying batch GD
+np.random.seed(42)
 lr = 0.1
 n_iterations = 1000
 m = 100
@@ -37,3 +40,51 @@ for iternation in range(n_iterations):
     gradients = 2/m * x_b.T.dot(x_b.dot(theta) - y)
     theta = theta - lr * gradients
 print('value at GD steps \n', theta)
+#plotting 
+theta_path_bgd = []
+
+def plot_gradient_descent(theta, eta, theta_path=None):
+    m = len(x_b)
+    plt.plot(x, y, "b.")
+    n_iterations = 1000
+    for iteration in range(n_iterations):
+        if iteration < 10:
+            y_predict = x_new_b.dot(theta)
+            style = "b-" if iteration > 0 else "r--"
+            plt.plot(x_new, y_predict, style)
+        gradients = 2/m * x_b.T.dot(x_b.dot(theta) - y)
+        theta = theta - eta * gradients
+        if theta_path is not None:
+            theta_path.append(theta)
+    plt.xlabel("$x_1$", fontsize=18)
+    plt.axis([0, 2, 0, 15])
+    plt.title(r"$\eta = {}$".format(eta), fontsize=16)
+
+plt.figure(figsize=(10,4))
+plt.subplot(131); plot_gradient_descent(theta, eta=0.02)
+plt.ylabel("$y$", rotation=0, fontsize=18)
+plt.subplot(132); plot_gradient_descent(theta, eta=0.1, theta_path=theta_path_bgd)
+plt.subplot(133); plot_gradient_descent(theta, eta=0.5)
+plt.show()
+
+## applying SGD
+n_epochs = 50
+t0, t1 = 5, 50 # learning schedule parameters
+def learning_schedule(t):
+    return t0 / (t+t1)
+
+theta = np.random.randn(2,1)
+
+for epoch in range(n_epochs):
+    for i in range(m):
+        random_index = np.random.randint(m)
+        xi = x_b[random_index:random_index+1]
+        yi = y[random_index:random_index+1]
+        gradients = 2 * xi.T.dot(xi.dot(theta) - yi)
+        eta = learning_schedule(epoch * m + i)
+        theta = theta - eta * gradients
+print('SGD for 50 iterations: ', theta)
+
+sgd_reg = SGDRegressor(max_iter=1000, tol=1e-3, penalty=None, eta0=0.1) #eta0 is initial lr; tol is for early stopping
+sgd_reg.fit(x, y.ravel())
+print('SGD intercept: {} \n SGD coef: {}'.format(sgd_reg.intercept_, sgd_reg.coef_))
