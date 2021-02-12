86b90afff0747e27ef8aeb6691ecff423fa10e0c
rishi-wqd190004 <wqd190004@siswa.um.edu.my>
2021-02-12 14:11:34 +0800

    adding batch gradient descent

--- ML_learning/training_linear_models/linear_regression_normal_equation.py
+++ ML_learning/training_linear_models/linear_regression_normal_equation.py
@@ -13,7 +13,7 @@ plt.show()
 # computing the normal equation
 x_b = np.c_[np.ones((100,1)), x]
 theta_best = np.linalg.inv(x_b.T.dot(x_b)).dot(x_b.T).dot(y)
-print('theta best: \n', theta_best)
+print('theta best with normal equation: \n', theta_best)
 # making predictions
 x_new = np.array([[0], [2]])
 x_new_b = np.c_[np.ones((2,1)), x_new]
@@ -27,3 +27,13 @@ plt.xlabel('X')
 plt.ylabel('Y', rotation=0)
 plt.axis([0, 2, 0, 15])
 plt.show()
+
+## applying batch GD
+lr = 0.1
+n_iterations = 1000
+m = 100
+theta = np.random.randn(2,1)
+for iternation in range(n_iterations):
+    gradients = 2/m * x_b.T.dot(x_b.dot(theta) - y)
+    theta = theta - lr * gradients
+print('value at GD steps \n', theta)
