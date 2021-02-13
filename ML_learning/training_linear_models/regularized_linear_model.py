bfdc4b1477d458b97b500b47f72d6847f0751189
rishi-wqd190004 <wqd190004@siswa.um.edu.my>
2021-02-14 02:16:45 +0800

    adding lasso regression

--- ML_learning/training_linear_models/regularized_linear_model.py
+++ ML_learning/training_linear_models/regularized_linear_model.py
@@ -1,14 +1,26 @@
 import numpy as np
-from sklearn.linear_model import Ridge
+from sklearn.linear_model import Ridge, SGDRegressor, Lasso
 
 ## Ridge regression
-### on closed-form solution
+### by closed-form solution
 np.random.seed(42)
 m = 20
 x = 3 * np.random.rand(m, 1)
 y = 1 + 0.5 * x + np.random.randn(m, 1) / 1.5
 x_new = np.linspace(0,3,100).reshape(100,1)
-ridge_reg = Ridge(alpha=1, solver="cholesky", random_state=42)
+ridge_reg = Ridge(alpha=1, solver="cholesky", random_state=42) # can use solver="sag" which is stochastic average GD
 ridge_reg.fit(x, y)
 ridge_reg.predict([[1.5]])
-print('Ridge on closed-form solution for 1.5: \n', ridge_reg.predict([[1.5]]))
+print('Ridge by closed-form solution for 1.5: \n', ridge_reg.predict([[1.5]]))
+
+### by gradient descent
+sgd_reg = SGDRegressor(penalty="l2", max_iter=1000, tol=1e-3, random_state=42)
+sgd_reg.fit(x, y.ravel())
+sgd_reg.predict([[1.5]])
+print('Ridge by Gradient decent for 1.5: \n', sgd_reg.predict([[1.5]]))
+
+
+## Lasso regression
+lasso_reg = Lasso(alpha=0.1)
+lasso_reg.fit(x, y)
+print("Lasso regression predict for 1.5: \n", lasso_reg.predict([[1.5]]))
